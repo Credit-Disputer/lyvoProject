32ca0db48e1821529a60a347caf2a4ee49f085b8
Vishal Sharma <makeavish786@gmail.com>
2020-07-04 17:41:58 +0530

    Edited data models and added templates

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,7 +17,43 @@ const UserSchema = new mongoose.Schema({
   register_date: {
     type: Date,
     default: Date.now
-  }
+  },
+  street_add:{
+    type: String,
+    required: true
+  },
+  ssn:{
+    type: Int32Array,
+    required: true
+  },
+  city:{
+    type: String,
+    required: true
+  },
+  state:{
+    type: String,
+    required: true
+  },
+  zip:{
+    type:Int32Array,
+    required:true
+  },
+  dob:{
+    type:Date,
+    required:true
+  },
+  equifax:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'Equifax'
+  }],
+  experian:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'experian'
+  }],
+  transunion:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'transunion'
+  }]
 });
 
 module.exports = User = mongoose.model("RegisterUser", UserSchema);
